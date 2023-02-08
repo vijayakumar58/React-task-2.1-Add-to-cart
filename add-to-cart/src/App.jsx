a7b6d88bd55459ede6e3c33908435d1aff0a75ea
vijayakumar58 <vijayalingam58@gmail.com>
2023-02-08 19:25:53 +0530

    add

--- add-to-cart/src/App.jsx
+++ add-to-cart/src/App.jsx
@@ -7,50 +7,50 @@ import { useState } from 'react';
 //step-2 create the app in add to cart , 
 function App() {
   let products = [
-    { 
-      id:"1",
+    {
+      id: "1",
       image: "https://media.istockphoto.com/id/502129687/photo/decorative-cosmetics-for-makeup.jpg?s=1024x1024&w=is&k=20&c=wGmovDLR3scv2NWrha6VGUO82qn_cWwxBh1ljemwo-0=",
       title: "Fancy Products",
       price: 4540.00
     },
     {
-      id:"2",
+      id: "2",
       image: "https://m.media-amazon.com/images/I/71u3QxI3QvL._UY695_.jpg",
       title: "Special Item",
       price: 1458.00
     },
     {
-      id:"3",
+      id: "3",
       image: "https://m.media-amazon.com/images/I/71Ye4QY493L._SX679_.jpg",
       title: "Male Puppets",
       price: 2025.00
     },
-    { 
-      id:"4",
+    {
+      id: "4",
       image: "https://m.media-amazon.com/images/I/413LMwScBjS._SX679_.jpg",
       title: "Wifi Booster",
       price: 3240.00
     },
-    { 
-      id:"5",
+    {
+      id: "5",
       image: "https://m.media-amazon.com/images/I/81wN5ep5CAL._SY879_.jpg",
       title: "JH Lotus Hangings",
       price: 2025.00
     },
     {
-      id:"6",
+      id: "6",
       image: "https://m.media-amazon.com/images/I/71+FRIdP3pL._UL1500_.jpg",
       title: "Indian Kanjivaram Silk",
       price: 16720.00
     },
     {
-      id:"7",
+      id: "7",
       image: "https://m.media-amazon.com/images/I/61xlUgke8BL._UX679_.jpg",
       title: "Men's Sherwani Set",
       price: 1458.00
     },
     {
-      id:"8",
+      id: "8",
       image: "https://m.media-amazon.com/images/I/616e2t492uL._SX679_.jpg",
       title: "Alpha Smart Watch ",
       price: 3240
@@ -65,15 +65,24 @@ function App() {
   };
   //using findindex remove the values of add to cart
   let removeFromCart = (item) => {
-    let index = cart.findIndex ((obj)=>obj.id === item.id);
-    cart.splice(index,1);
+    let index = cart.findIndex((obj) => obj.id === item.id);
+    cart.splice(index, 1);
     setCart([...cart]);
-    setTotal(total-item.price);
+    setTotal(total - item.price);
   }
 
-  
-  return (
-    //step-3 create container with using column split to page in col-lg-8 is card datas , col-lg-4 is addtocard details
+
+  return (<>
+  {/* create using header */}
+    <header className="bg-primary py-5 mb-2">
+      <div className="container px-4 px-lg-5 my-5">
+        <div className="text-center text-warning">
+          <h1 className="display-4 fw-bolder">OIP Shoping Platform</h1>
+          <p className="lead fw-normal text-warning-100 mb-0">Best Product Best Quality</p>
+        </div>
+      </div>
+    </header>
+    {/* //step-3 create container with using column split to page in col-lg-8 is card datas , col-lg-4 is addtocard details */}
     <div className="container">
       <div className="row">
         <div className="col-lg-8" >
@@ -89,7 +98,7 @@ function App() {
         </div>
         <div className='col-lg-4'>
           {
-            cart.length === 0 ? <div> No Items in Cart </div> : <div className='row'>
+            cart.length === 0 ? <div> <b>No Items in Cart </b></div> : <div className='row'>
               <ol className="list-group list-group-numbered">
                 {/* step-5 using Cart component,usestate using map */}
                 {cart.map((cartItem) => {
@@ -103,7 +112,7 @@ function App() {
       </div>
 
     </div>
-  );
+  </>);
 }
 
 export default App;
